6f3d621ba833b174711f43a7380b87cc85402b6f
zeromotivat1on <lyannoy.alexander@gmail.com>
2021-05-31 18:34:47 +0300

    btns animation blinkin' and arrow btn scroll on click and appear animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,26 @@
-const footer = document.querySelector('.footer')
-console.log(footer)
-
 const arrowDown = document.querySelector('.arrow-down')
-
-let clockwiseRotation = true
+let clockwiseRotation = true,
+    arrowPointsDown = true // check arrow point direction in order to scroll in correct way, while its clicked
 window.addEventListener('scroll', () => {
     let scrollPos = this.scrollY
-    console.log(clockwiseRotation)
-
-    if(scrollPos !== 0) arrowDown.style.transform = 'rotate(180deg)'
-    
-    if(scrollPos === 0 && clockwiseRotation) {
-        arrowDown.style.transform = 'rotate(0deg)'
-        clockwiseRotation = false
-    } else if(scrollPos === 0 && !clockwiseRotation) {
-        arrowDown.style.transform = 'rotate(360deg)'
-        clockwiseRotation = true
+    if(scrollPos !== 0) { // if we scrolled - rotate arrow button
+        arrowDown.style.transform = 'rotate(180deg)'
+        arrowPointsDown = false
+    } else {
+        // opposite (clockwise and anti-clockwise) rotations each iteration
+        if(clockwiseRotation) {
+            arrowDown.style.transform = 'rotate(0deg)'
+            clockwiseRotation = false
+            arrowPointsDown = true
+        } else {
+            arrowDown.style.transform = 'rotate(360deg)'
+            clockwiseRotation = true
+            arrowPointsDown = true
+        }
     }
+})
+
+arrowDown.addEventListener('click', () => {
+    if(arrowPointsDown) window.scrollBy(0, window.innerHeight)
+    else window.scrollBy(0, -window.innerHeight)
 })
