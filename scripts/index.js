94289e785d49c1f2b55378724982f54cec237b62
zeromotivat1on <lyannoy.alexander@gmail.com>
2021-06-01 19:33:45 +0300

    page transition, about page start

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+window.onload = () => {
+    includeHTML() // function to include part of html code
+
+    // Page transition
+    const transitionElem = document.querySelector('.transition')
+    setTimeout(() => {
+        transitionElem.classList.remove('is-active')
+    }, 500)
+
+    // Following part of code rewrites the logic of anchors in order to
+    // make page transition not only while loading, but while going through ref
+    const anchors = document.querySelectorAll('a')
+    for(let i in anchors) {
+        anchors[i].addEventListener('click', (e) => {
+            e.preventDefault()
+            let target = e.target.href
+            if(target === undefined) target = 'index.html'
+            transitionElem.classList.add('is-active')
+
+            setTimeout(() => {
+                window.location.href = target
+            }, 500)
+        })
+    }
+}
+
 const arrowDown = document.querySelector('.arrow-down')
 let clockwiseRotation = true,
     arrowPointsDown = true // check arrow point direction in order to scroll in correct way, while its clicked
